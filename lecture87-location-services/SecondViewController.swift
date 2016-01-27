2f4e192eef856cbd42669934a41da11e8244c629
Ryan <ryanhuebert@gmail.com>
2016-01-27 08:57:57 -0800

    Initial Commit

---
+++ lecture87-location-services/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  lecture87-location-services
+//
+//  Created by Ryan Huebert on 1/27/16.
+//  Copyright © 2016 Ryan Huebert. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
